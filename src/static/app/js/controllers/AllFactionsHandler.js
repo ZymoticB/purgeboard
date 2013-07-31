d17c3125d22039322d092ea6f279600010ead2dc
Tyler Dixon <tyler@chlystyk.com>
2013-07-31 05:33:28 +0000

    Add some prettyness

--- src/static/app/js/controllers/AllFactionsHandler.js
+++ src/static/app/js/controllers/AllFactionsHandler.js
@@ -13,11 +13,13 @@ purgeboard.controller('AllFactionsHandler',
 		var today = new Date();
 		var year = today.getUTCFullYear();
 		var month = today.getUTCMonth() + 1; //why the fuck would you return 0-11 ...
-		var day = today.getUTCDate() - 1;
-		$scope.yesterday = year+month.pad(2)+day.pad(2);
-		$scope.current_day_stats = AllFactionsDaily.query({day: $scope.yesterday});
+		var day = today.getUTCDate() - 2; //UTC will bump this up a day in most TZs and data doesn't come until 11:00UTC the day after
+		var yesterday = year+month.pad(2)+day.pad(2);
+		$scope.stats = {yesterday: yesterday};
+		var current_day_stats = AllFactionsDaily.query({day: $scope.stats.yesterday});
+		$scope.stats.current_day_stats = current_day_stats;
 		$scope.update_stats = function(){
-			$scope.current_day_stats = AllFactionsDaily.query({day: $scope.yesterday});
+			$scope.stats.current_day_stats = AllFactionsDaily.query({day: $scope.stats.yesterday});
 		};
 	}
 );
